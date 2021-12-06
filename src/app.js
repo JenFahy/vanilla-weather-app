09346d4f189d7daa5a955ed329d4fc2cbc64741c
JenFahy <94325811+JenFahy@users.noreply.github.com>
2021-12-05 21:39:19 -0500

    Cleaned JS: Remove console log, update apiUrl

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -40,6 +39,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "31591cbd759ed47dd528c71d7a08734d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
+let city = "Toronto";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
