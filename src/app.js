d18033f02ecc4e1811306c341e846640f8307abf
JenFahy <94325811+JenFahy@users.noreply.github.com>
2021-12-12 16:14:37 -0500

    Integrated API into forecast

--- src/app.js
+++ src/app.js
@@ -23,29 +23,45 @@ function formatDate(timestamp) {
   //make local time later
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data);
+  let forecastArray = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHtml = `<div class="row">`;
-  let days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+
+  forecastArray.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHtml =
+        forecastHtml +
+        `
             <div class="col-2">
               <div class="weather-forecast-date">
-              ${day} </div><img
-                  src="https://openweathermap.org/img/wn/03d@2x.png"
-                  alt="cloudy"
+              ${formatDay(forecastDay.dt)} </div><img
+                  src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
+                  alt=""
                   id="weather-icon"
                   width="42"
                 />
                 <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max">18°C</span>
-                  <span class="weather-forecast-temperature-min">12°C</span>
+                  <span class="weather-forecast-temperature-max">${Math.round(
+                    forecastDay.temp.max
+                  )}°C</span>
+                  <span class="weather-forecast-temperature-min">${Math.round(
+                    forecastDay.temp.min
+                  )}°C</span>
                 </div>
                             </div>`;
+    }
   });
 
   forecastHtml = forecastHtml + `</div>`;
